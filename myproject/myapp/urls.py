9b7d2986866f891680ac59f52bf27fa96da3f7ce
amilsha <amilshahannath7@gmail.com>
2025-02-09 09:33:10 +0530

    commit

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('homer/', views.restaurant1, name='restaurant1'),
     path('', views.home, name='home'),
+    path('homer2/', views.foodstreet, name='foodstreet'),
+    path('homer3/', views.luxurydining, name='luxurydining'),
+    path('shoppingmall1/', views.shoppingmall1, name='shoppingmall1'),
+    path('marketplace/', views.marketplace, name='marketplace'),
+    path('luxurymall/', views.luxurymall, name='luxurymall'),
 ]
+
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -5,4 +5,20 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'myapp/home.html')
+    print('hey')
+    return render(request, 'home.html')
+def restaurant1(request):
+    return render(request, 'restaurant1.html')
+def foodstreet(request):
+    return render(request, 'foodstreet.html')
+def luxurydining(request):
+    return render(request, 'luxurydining.html')
+def shoppingmall1(request):
+    return render(request, 'shoppingmall1.html')
+def marketplace(request):
+    return render(request, 'marketplace.html')
+def luxurymall(request):
+    return render(request, 'luxurymall.html')
+
+
+
